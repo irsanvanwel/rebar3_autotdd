0768d83052cf30e9aa7a6fa74ef54f795e1a8289
Irsan van Wel <irsanvanwel@local.dev>
2017-11-17 09:02:20 +0200

    Signed-off-by: Irsan van Wel <irsanvanwel@local.dev>

---
+++ README.md
@@ -0,0 +1,45 @@
+rebar_autotdd_plugin
+=====
+
+A rebar3 plugin for auto running compile on source file change reloading modules in the shell.
+
+Prerequisite
+-----
+On Linux you need to install inotify-tools.
+
+
+Use
+---
+
+Add the plugin only to your user local rebar config in `~/.config/rebar3/rebar.config`:
+
+    {plugins, [rebar3_auto]}.
+
+If you add it to your project rebar.config, it will get unloaded each time compilation occurs, thus breaking it.
+
+Then run
+```
+    $ rebar3 compile
+```
+
+Then just call your plugin directly in an existing application:
+
+
+```
+(relx) $ rebar3 auto
+===> Compiling rebar3_auto
+Setting up watches.  Beware: since -r was given, this may take a while!
+Watches established.
+Erlang/OTP 18 [erts-7.1] [source] [64-bit] [smp:4:4] [async-threads:0] [hipe] [kernel-poll:false]
+
+Eshell V7.1  (abort with ^G)
+1> ===> This feature is experimental and may be modified or removed at any time.
+Compiling rebar3_auto
+Verifying dependencies...
+Compiling relx
+Compiling rebar3_auto
+Verifying dependencies...
+Compiling relx
+
+1>
+```
---
+++ src/rebar3_autotdd.erl
@@ -0,0 +1,147 @@
+%% @doc
+%% Add the plugin to your rebar config, since it is a developer tool and not
+%% necessary for building any project you work on I put it in
+%% `~/config/.rebar3/rebar.config`:
+%%
+%% ```
+%% {plugins, [rebar3_autotdd]}.'''
+%%
+%% Then just call your plugin directly in an existing application:
+%%
+%% ```
+%% $ rebar3 auto
+%% ===> Fetching rebar_auto_plugin
+%% ===> Compiling rebar_auto_plugin'''
+%%
+-module(rebar3_autotdd).
+-behaviour(provider).
+
+-export([init/1
+        ,do/1
+        ,format_error/1]).
+
+-export([auto/0, flush/0]).
+
+-define(PROVIDER, auto).
+-define(DEPS, [compile]).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+            {name, ?PROVIDER},        % The 'user friendly' name of the task
+            {module, ?MODULE},        % The module implementation of the task
+            {bare, true},             % The task can be run by the user, always true
+            {deps, ?DEPS},            % The list of dependencies
+            {example, "rebar3 autotdd"}, % How to use the plugin
+            {opts, [{config, undefined, "config", string,
+                     "Path to the config file to use. Defaults to "
+                     "{shell, [{config, File}]} and then the relx "
+                     "sys.config file if not specified."},
+                    {name, undefined, "name", atom,
+                     "Gives a long name to the node."},
+                    {sname, undefined, "sname", atom,
+                     "Gives a short name to the node."},
+                    {setcookie, undefined, "setcookie", atom,
+                     "Sets the cookie if the node is distributed."},
+                    {script_file, undefined, "script", string,
+                     "Path to an escript file to run before "
+                     "starting the project apps. Defaults to "
+                     "rebar.config {shell, [{script_file, File}]} "
+                     "if not specified."},
+                    {apps, undefined, "apps", string,
+                     "A list of apps to boot before starting the "
+                     "shell. (E.g. --apps app1,app2,app3) Defaults "
+                     "to rebar.config {shell, [{apps, Apps}]} or "
+                     "relx apps if not specified."}]},
+            {short_desc, "Automatically run compile task on change of source file and reload modules."},
+            {desc, ""}
+    ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec format_error(any()) ->  iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    spawn(fun() ->
+            listen_on_project_apps(State),
+            ?MODULE:auto()
+        end),
+    State1 = remove_from_plugin_paths(State),
+    rebar_prv_shell:do(State1).
+
+-define(VALID_EXTENSIONS,[<<".erl">>, <<".hrl">>, <<".src">>, <<".config">>, <<".lock">>,
+    <<".c">>, <<".cpp">>, <<".h">>, <<".hpp">>, <<".cc">>]).
+
+auto() ->
+    case whereis(rebar_agent) of
+        undefined ->
+            timer:sleep(100);
+
+        _ ->
+            receive 
+                {ChangedFile, _Events} ->
+                    Ext = filename:extension(unicode:characters_to_binary(ChangedFile)),
+                    IsValid = lists:member(Ext, ?VALID_EXTENSIONS),
+                    case IsValid of
+                        false -> pass;
+                        true ->
+                            % sleep here so messages can bottle up
+                            % or we can flush after compile?
+                            timer:sleep(200),
+                            flush(),
+                            rebar_agent:do(compile),
+                            rebar_agent:do(eunit)
+                    end;
+                _ -> pass
+            end
+
+    end,
+    ?MODULE:auto().
+
+flush() ->
+    receive
+        _ ->
+            flush()
+    after
+        0 -> ok
+    end.
+
+listen_on_project_apps(State) ->
+    CheckoutDeps = [AppInfo || 
+        AppInfo <-rebar_state:all_deps(State), 
+        rebar_app_info:is_checkout(AppInfo) == true
+    ],
+    ProjectApps = rebar_state:project_apps(State),
+    lists:foreach(
+        fun(AppInfo) ->
+            SrcDir = filename:join(rebar_app_info:dir(AppInfo), "src"),
+            CSrcDir = filename:join(rebar_app_info:dir(AppInfo), "c_src"),
+            case filelib:is_dir(SrcDir) of
+                true -> enotify:start_link(SrcDir);
+                false -> ignore
+            end,
+            case filelib:is_dir(CSrcDir) of
+                true -> enotify:start_link(CSrcDir);
+                false -> ignore
+            end
+        end, 
+        ProjectApps ++ CheckoutDeps
+    ).
+
+remove_from_plugin_paths(State) ->
+    PluginPaths = rebar_state:code_paths(State, all_plugin_deps),
+    PluginsMinusAuto = lists:filter(
+        fun(Path) ->
+            Name = filename:basename(Path, "/ebin"),
+            not (list_to_atom(Name) =:= rebar_auto_plugin
+                orelse list_to_atom(Name) =:= enotify)
+        end, 
+        PluginPaths
+    ),
+    rebar_state:code_paths(State, all_plugin_deps, PluginsMinusAuto).
